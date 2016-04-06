8fe1b95f360ca27ee2e9efc1fa4b8a85838228a0
Bruno Garcia <029141072@unifacs.edu.br>
2016-04-06 18:36:05 -0300

    editando a classe bdhelper

--- app/src/main/java/br/com/brdev/listacompras/BdHelper.java
+++ app/src/main/java/br/com/brdev/listacompras/BdHelper.java
@@ -4,21 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by brgarcia on 06/04/2016.
- */
 public class BdHelper extends SQLiteOpenHelper{
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 2;              //versao do db
+    private static final String DATABASE_NAME = "PRODUTOS";     //nome do db
+    private static final String DICTIONARY_TABLE_CREATE =       // texto padrao de criacao do db
+            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
+            " NOME TEXT, " +
+            " VALOR TEXT);";
 
-
-    DatabaseHelper (Context context) {
+    public BdHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DICTIONARY_TABLE_CREATE);
 
     }
 
--- app/src/main/java/br/com/brdev/listacompras/Main.java
+++ app/src/main/java/br/com/brdev/listacompras/Main.java
@@ -1,13 +1,20 @@
 package br.com.brdev.listacompras;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+    Button
+        buttonCadastrarProduto,
+        buttonConsultarPreco,
+        buttonMontarListaCompras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
     }
 }
