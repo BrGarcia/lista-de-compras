3fc92c4dee4a08497e64a0e59bc7718ec7240f6f
Bruno Garcia <brunodog@gmail.com>
2016-04-07 14:20:52 -0300

    Update BdHelper.java
    alteracoes na classe dbhelper

--- app/src/main/java/br/com/brdev/listacompras/BdHelper.java
+++ app/src/main/java/br/com/brdev/listacompras/BdHelper.java
@@ -5,26 +5,33 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BdHelper extends SQLiteOpenHelper{
+    
+    // Informacoes da tabela
+    public static final String TABLE_PRODUTO = "produtos";
+    public static final String PRODUTO_ID = "_id";
+    public static final String PRODUTO_NOME = "nome";
 
-    private static final int DATABASE_VERSION = 2;              //versao do db
-    private static final String DATABASE_NAME = "PRODUTOS";     //nome do db
-    private static final String DICTIONARY_TABLE_CREATE =       // texto padrao de criacao do db
-            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
-            " NOME TEXT, " +
-            " VALOR TEXT);";
+    // informacoes da basede dados
+    static final String DB_NOME = "DB_PRODUTO";
+    static final int DB_VERSAO = 1;
+
+    private static final String CREATE_TABLE = "create table "
+            + TABLE_PRODUTO + "(" + PRODUCTO_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + PRODUTO_NOME + " TEXT NOT NULL);";
 
     public BdHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, DB_NOME, null,DB_VERSAO);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DICTIONARY_TABLE_CREATE);
-
+        db.execSQL(CREATE_TABLE);
     }
 
-    @Override
+     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCT);
+        onCreate(db);
     }
 }
